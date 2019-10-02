55aeda597662babf833b3ae1d2c993fc2e65ab63
b3-kennedy <benjamin3.kennedy@live.uwe.ac.uk>
2019-10-02 09:51:30 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Welcome to my program"<<std::endl;
+
+
+    int a = 0;
+    int b = 3;
+
+    std::cout << "Please enter a number: ";
+    std::cin >> a;
+    std::cout << "Enter a second number ";
+    std::cin >> b;
+
+
+
+    std::cout << "Your number is " << a + b << std::endl;
+    //testing git
+    return 0;
+}
